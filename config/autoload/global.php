643ef88a882d2691b6c9fd99a9f7bb5223eaee45
Akhmedov Butta <b.akhmedov@rambler-co.ru>
2018-11-02 10:36:59 +0300

    first

---
+++ config/autoload/global.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Global Configuration Override
+ *
+ * You can use this file for overriding configuration values from modules, etc.
+ * You would place values in here that are agnostic to the environment and not
+ * sensitive to security.
+ *
+ * @NOTE: In practice, this file will typically be INCLUDED in your source
+ * control, so do not include passwords or other sensitive information in this
+ * file.
+ */
+
+use Zend\Log\Writer\Stream;
+
+return [
+    'isDebug' => true,
+	'db' => [
+		'driver'         => 'Pdo',
+		'dsn'            => 'mysql:dbname=igp;host=localhost',
+		'username'       => 'bootta',
+		'password'       => '1991',
+		'driver_options' => [
+			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+		],
+	],
+
+    'log' => [
+        'writers' => [
+            //Можем добавить несколько стримов.
+            ['name' => new Stream(__DIR__.'/../../log/web.igp.wp.log'),]
+        ],
+    ],
+
+    'cache' => [
+        'adapter' => [
+            'name' => 'memcached',
+            'options' => [
+                'namespace' => 'methods',
+                'ttl' => 30,
+                'servers' => [
+                    ['127.0.0.1', 11211],
+                ],
+            ],
+        ],
+    ],
+];
---
+++ config/modules.config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Zend\\Db',
+	'Zend\\Router',
+	'Zend\\Validator',
+//    'Zend\\Di',
+    'Zend\\Log',
+//    'Zend\\Cache',
+
+	'Application',
+];
+
+?>
